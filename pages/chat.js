fa2a78a36d66c627fe2758e1ecc7580f361fcaed
JIANG John <1249036364@qq.com>
2023-07-30 22:19:46 +0200

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react';
 
 const ChatPage = () => {
   useEffect(() => {
-    // This will be executed after the component is mounted
     var chatConfig = {
       token: 'oIrehxj3FK7hUFc0eU4X',
     };
