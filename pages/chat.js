b9cdad1d010080622bff159f720f1c7f76aeb8cc
JIANG John <1249036364@qq.com>
2023-07-30 22:15:26 +0200

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,9 +1,17 @@
-<script src="https://cloud.openchat.so/chat.js"></script>
-<script>
-document.addEventListener('DOMContentLoaded', function() {
+// pages/chat.js
+
+import React, { useEffect } from 'react';
+
+const ChatPage = () => {
+  useEffect(() => {
+    // This will be executed after the component is mounted
     var chatConfig = {
-    token: "oIrehxj3FK7hUFc0eU4X",
+      token: "oIrehxj3FK7hUFc0eU4X",
     };
     initializeChatWidget(chatConfig);
-});
-</script>
+  }, []); // The empty dependency array ensures that this effect runs only once after initial mount
+
+  return <div>Chat Widget is loading...</div>;
+};
+
+export default ChatPage;
