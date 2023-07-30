76eafdb19ee5726a755cbfdb07676efa12737f9c
JIANG John <1249036364@qq.com>
2023-07-30 22:11:33 +0200

    Create chat.js

---
+++ pages/chat.js
@@ -0,0 +1,9 @@
+<script src="https://cloud.openchat.so/chat.js"></script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var chatConfig = {
+    token: "oIrehxj3FK7hUFc0eU4X",
+    };
+    initializeChatWidget(chatConfig);
+});
+</script>
