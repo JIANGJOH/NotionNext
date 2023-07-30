2c210ec57314f4b67006fac388f64e26d81670f5
JIANG John <1249036364@qq.com>
2023-07-30 22:17:37 +0200

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -6,7 +6,7 @@ const ChatPage = () => {
   useEffect(() => {
     // This will be executed after the component is mounted
     var chatConfig = {
-      token: "oIrehxj3FK7hUFc0eU4X",
+      token: 'oIrehxj3FK7hUFc0eU4X',
     };
     initializeChatWidget(chatConfig);
   }, []); // The empty dependency array ensures that this effect runs only once after initial mount
