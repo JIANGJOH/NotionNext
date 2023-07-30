883f02badee2384f68cb1d1307ece9ee80982f21
JIANG John <1249036364@qq.com>
2023-07-30 22:54:56 +0200

    Create chat.js

---
+++ pages/chat.js
@@ -0,0 +1,16 @@
+// pages/chat.js
+
+import React, { useEffect } from 'react';
+
+const ChatPage = () => {
+  useEffect(() => {
+    const chatConfig = {
+      token: 'oIrehxj3FK7hUFc0eU4X',
+    };
+    initializeChatWidget(chatConfig);
+  }, []); // The empty dependency array ensures that this effect runs only once after initial mount
+
+  return <div>Chat Widget is loading...</div>;
+};
+
+export default ChatPage;
